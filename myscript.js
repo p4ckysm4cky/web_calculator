233f4a23c8b9a7a749f41dc7a156dec805efcf9d
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2021-07-18 18:57:37 +1200

    Added DOM objects to js

---
+++ myscript.js
@@ -0,0 +1,14 @@
+const equationOutput = document.getElementById("equation");
+const answerOutput = document.getElementById("answer");
+
+
+const clearBtn = document.getElementById("ac");
+const delBtn = document.getElementById("del");
+const divideBtn = document.getElementById("divide");
+const multiplyBtn = document.getElementById("multiply");
+const minusBtn = document.getElementById("minus");
+const plusBtn = document.getElementById("plus");
+const equalBtn = document.getElementById("equal");
+const numBtns = document.getElementsByClassName("num")
+
+
